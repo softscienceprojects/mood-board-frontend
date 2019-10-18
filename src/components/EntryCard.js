5e4d6a9f028f7da88fb7424ed422d99cb967a8af
tegansims <github email address>
2019-10-18 11:34:13 +0100

    semantic installed

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import { Card } from 'semantic-ui-react'
 
 class EntryCard extends React.Component {
 
     render(){
         return <div>
             this is entry card
+            <Card>
+            <div className="content">
+            <div className="header">{this.props.entry.message}</div>
+          </div>
+            </Card>
+            
         </div>
     }
 
