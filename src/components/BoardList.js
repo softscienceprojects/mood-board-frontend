d79566c760157fb01c5ad37d3e5ee685d2021ea7
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-19 09:29:41 +0100

    added key

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -6,7 +6,7 @@ class BoardList extends React.Component {
     render(){
         return <div>
             this is BoardList
-            {this.props.entries.map(entry => <EntryCard entry={entry}/>)}
+            {this.props.entries.map(entry => <EntryCard key={entry.id} entry={entry}/>)}
             
         </div>
     }
