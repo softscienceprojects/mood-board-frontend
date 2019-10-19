3048d8e8f34b7d9b8f8ebed192ddc040fb65605a
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-19 10:35:33 +0100

    set up SVG to hold color

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -2,21 +2,29 @@ import React, {Component} from 'react';
 import { Card } from 'semantic-ui-react'
 
 class EntryCard extends React.Component {
-
-    state = ({
+    
+    state = {
         cardType: true,
-        color: 'yellow'
-    //    color: `#${this.props.entry.colours}`
-    })
+       // color: 'yellow'
+        color: this.props.entry.colours
+    }
 
     changeCardType=()=>{
         this.setState({ cardType: !this.state.cardType })
+    }
+
+    colorSq = () => {
+        let rect = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        rect.setAttribute('width',500);
+        rect.setAttribute('height',500);
+        rect.setAttribute('fill','#95B3D7');    
     }
 
     render(){
         return <div>
             <Card.Group itemsPerRow={4}>
             <Card color={this.state.color}>
+            {/* <Card> */}
             <div className="content"  onClick={this.changeCardType}>
                 <div className="header" >{this.state.cardType? this.props.entry.colours : this.props.entry.message} </div>
             </div>
