f76fcb9132c037463e6e31f19693c7eb433f5eac
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-21 11:48:46 +0100

    remove svg and refactor card color

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -13,17 +13,12 @@ class EntryCard extends React.Component {
         this.setState({ cardType: !this.state.cardType })
     }
 
-    colorSq = () => {
-        let rect = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        rect.setAttribute('width',500);
-        rect.setAttribute('height',500);
-        rect.setAttribute('fill','#95B3D7');    
-    }
 
     render(){
+        let colourString = { backgroundColor: this.state.color }
         return <div>
             <Card.Group itemsPerRow={4}>
-            <Card color={this.state.color}>
+            <Card color={this.state.color} >
             {/* <Card> */}
             <div className="content"  onClick={this.changeCardType}>
                 <div className="header" >{this.state.cardType? this.props.entry.colours : this.props.entry.message} </div>
