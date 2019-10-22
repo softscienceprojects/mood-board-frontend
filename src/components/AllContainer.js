b7a2c599d67ec74c2596224bb71a753fc23ba571
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 17:44:01 +0100

    added search feature

--- src/components/AllContainer.js
+++ src/components/AllContainer.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 import BoardList from './BoardList'
+import { Search } from 'semantic-ui-react'
+
 
 class AllContainer extends React.Component {
+    state = {
+        results: [],
+      }
+
+    handleSearch = (e) => {
+        let search = new RegExp(e.target.value, "i")
+        let results = this.props.entries.filter(entry => entry.message.match(search))
+        this.setState({results})
+      }
 
     render(){
         return <div class="all-container">
-            This is AllContainer
-            <BoardList entries={this.props.entries}/>
+            <Search onSearchChange={this.handleSearch} label="search" showNoResults={false} placeholder="filter messages..."/>
+            <br />
+            <BoardList entries={this.state.results.length === 0  ? this.props.entries : this.state.results }/>
+
         </div>
     }
 
