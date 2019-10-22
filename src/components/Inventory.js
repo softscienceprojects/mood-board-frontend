e30f1f12c99fc584b767e515b595ec0dee8c3102
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 09:22:20 +0100

    removed all previous code

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Inventory extends React.Component {
+
+    render(){
+        return <div class="all-container">
+            Inventory
+        </div>
+    }
+
+}
+
+export default Inventory;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,39 +6,7 @@ const BASE_URL = 'http://localhost:3000/'
 
 
 class LoginForm extends React.Component {
-    state ={
-        email: '',
-        password: ''
-    }
 
-  
-    // --- change state --- //
-    handleChange=(event)=>{
-        // console.log(searchTerms)
-        this.setState({
-        [event.target.name]: event.target.value
-        })
-    }  
-
-
-    handleSubmit=(event)=>{
-        event.preventDefault()
-        API.login({ email: this.state.email, password: this.state.password }).then(user => this.props.login(user))
-    }
-
-
-    render(){
-        return  <div>
-        <h3>Login</h3>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group widths="equal">
-            <Form.Input fluid label="Email" placeholder="Email" type="email" value={this.state.email} name="email" onChange={this.handleChange}/>
-            <Form.Input fluid label="Password" placeholder="password" type="password" value={this.state.password} name="password" onChange={this.handleChange}/>
-          </Form.Group>
-          <Form.Button>Submit</Form.Button>
-        </Form>
-      </div>
-    }
 
 }
 
