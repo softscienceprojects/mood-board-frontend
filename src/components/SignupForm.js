e19cedc69dce50664910b981bec761ec7ffbe816
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 18:12:24 +0100

    boilerplate starter code copied from signin

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,19 +1,66 @@
 import React, {Component} from 'react';
-import { Form } from 'semantic-ui-react'
+import { Form, Button } from 'semantic-ui-react'
 import API from '../adaptors/API'
 
 
+class SignupForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    password_confirmation: ''
+  }
 
-class SignupForm extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault()
+    
+    API.signIn(this.state)
+      .then(data => {
+        if (data.error) {
+          throw Error(data.error)
+        } else {
+          this.props.signIn(data)
+          this.props.history.push('/')   // CHANGE THIS URL TO WHATEVER YOU WANT TO REDIRECT TO WHEN SIGNED IN
+        }
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }
 
-    render(){
-        return (
-            <div>
-                    hello this is SIGN UP form
-            </div>  
-        )  
-    }
-  
+  handleChange = event =>
+    this.setState({ [event.target.name]: event.target.value })
+
+
+  render () {
+    const { email, password } = this.state
+    const { handleChange, handleSubmit } = this
+
+    return (
+        <Form onSubmit={handleSubmit}>
+     
+       <input type='text'
+          id='emailInput'
+          label='Email'
+          value={email}
+          onChange={handleChange}
+          name='email'
+          placeholder='email'
+        />
+        <br />
+         <input type='text'
+          id='passwordInput'
+          label='Password'
+          value={password}
+          onChange={handleChange}
+          name='password'
+          type='password'
+          placeholder='password'
+        />
+        <br />
+        <Button> Log In </Button>
+      </Form>
+    )
+  }
 }
 
 export default SignupForm;
