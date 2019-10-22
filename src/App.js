1bfac7542051751c27c2818bfd8a8641d3be84f7
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 09:22:06 +0100

    added switch router back in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main'
-
+import { Route, withRouter, Switch } from 'react-router-dom'
+import LoginForm from './components/LoginForm'
+import Inventory from './components/Inventory'
 
 function App() {
   return (
     <div className="App">
-      This is App
+      
+      <Switch>
+          <Route exact path='/' component={() => <h1>This is homepage</h1>} />
+          <Route
+            path='/signin'
+            component={routerProps => (
+              <LoginForm {...routerProps} signIn={this.signIn} />
+            )}
+          />
+          <Route
+            path='/inventory'
+            component={routerProps => (
+              <Inventory {...routerProps} username={this.state.username} />
+            )}
+          />
+          <Route component={() => <h1>Page not found.</h1>} />
+        </Switch>
+
      <Main />
     </div>
 
