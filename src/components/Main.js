a64e2bd94d2a366fc3c426dce1d5ddb9055daa5a
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 19:35:59 +0100

    entry form renders if current user

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import AllContainer from './AllContainer'
-//import EntryForm from './EntryForm';
+import EntryForm from './EntryForm';
 
 
 class Main extends Component {
    
 
     render(){
-        return <div className="main-container">
+        return (
+        <div className="main-container">
+        {this.props.currentUser ? <EntryForm pushNewEntryToState={this.props.pushNewEntryToState} filterCategories = {this.props.filterCategories} /> : null}
          <AllContainer entries={this.props.entries}  />
-        </div>
+        </div>)
     }
 
 }
