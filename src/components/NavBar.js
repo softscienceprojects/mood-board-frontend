882c68c616a506e17e7d621bec6a50fb4b561f11
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 18:12:09 +0100

    c/o not in use component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import EntryForm from './EntryForm'
 import { Button } from 'semantic-ui-react'
+// import EntryForm from './EntryForm'
 
-class NavBar extends React.Component {
+class NavBar extends Component {
 
     render(){
         return <div className="nav-bar">
            
-           <img src={process.env.PUBLIC_URL + 'moodboardlogo.png'} />
+           <img src={process.env.PUBLIC_URL + 'moodboardlogo.png'} alt="logo"/>
             {this.props.currentUser !== '' 
                 ? <Button onClick = {this.props.signOut} >Sign Out</Button>
                 : <Button onClick = {this.props.takeToSignInForm} >Log In</Button>
