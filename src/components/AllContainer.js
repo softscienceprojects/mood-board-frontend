e2955353d5d5286710489cb0e91f72e11c5a1256
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-23 13:50:23 +0100

    change placeholder text on filter options

--- src/components/AllContainer.js
+++ src/components/AllContainer.js
@@ -8,7 +8,7 @@ class AllContainer extends Component {
     render(){
         return <div className="all-container">
            <select className="ui fluid dropdown" name='category' id="category" onChange={this.props.handleFilterChange}>
-                        <option default>Category Filter</option>
+                        <option default>Filter by Category</option>
                         {this.props.filterCategories.map(filterCategory => (
                         <option key={filterCategory} value={filterCategory} >
                     {filterCategory}
@@ -17,7 +17,7 @@ class AllContainer extends Component {
             &nbsp;&nbsp;&nbsp;&nbsp;
             
             <Search onSearchChange={_.debounce(this.props.changeSearchTerms, 500, {leading: true, })} value={this.props.searchTerms} 
-            label="search" showNoResults={false} placeholder="filter messages..."/>
+            label="search" showNoResults={false} placeholder="Filter by message"/>
             <br />
             <BoardList entries={this.props.entries} />
                             
