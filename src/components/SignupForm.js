c05e091534ae024f0edf019e08c18213329cbdc1
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-23 09:22:34 +0100

    console.log for async checking

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -17,6 +17,8 @@ class SignupForm extends Component {
         if (data.error) {
           throw Error(data.error)
         } else {
+          console.log("data: ", data)
+          //API.validate()
           this.props.signIn(data)
           this.props.history.push('/you')   // CHANGE THIS URL TO WHATEVER YOU WANT TO REDIRECT TO WHEN SIGNED IN
         }
